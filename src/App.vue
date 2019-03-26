2a6558ceb4c51bb11e29ea1c4b4f7b09d3c49ed4
Yudjin <zhenyanat@gmail.com>
2019-03-26 08:20:08 +0300

    menu/logout butt/header

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,39 @@
   <div id="app">
     <link href='https://fonts.googleapis.com/css?family=Butcherman' rel='stylesheet'>
     <header>
-      <ul>
-        <li id="dropdown">
-          MENU
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+      <div class="underHeader">
+        <div id="name">
+          Project &#8810;GOLIATH&#8811;
+        </div>
+        <div style="width: 40%">
+        </div>
+        <div id="threeButt" style="display: inline-flex">
+          <div>
+            <router-link to="/login" class="firstButt roulink">LOGIN</router-link>
           </div>
-        </li>
-      </ul>
-      <div>Project &#8810;GOLIATH&#8811;</div>
-      <button id="buttLOG" @click="qwertY" v-if="divLogOut">LOG OUT</button>
+          <div>
+            <router-link to="/register" class="secondButt roulink">REGISTER</router-link>
+          </div>
+          <div>
+            <button class="thirdButt roulink"  @click="toggle=!toggle" >
+              MENU
+            </button>
+          </div>
+          <div id="hideMenu" v-show='toggle'>
+            <div>
+              <router-link to="/lol" class="roulink">Lol</router-link>
+            </div>
+            <div>
+              <router-link to="/choose" class="roulink">CHOOSE</router-link>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button id="buttLOG" v-if="divLogOut">
+            LOG OUT
+          </button>
+        </div>
+      </div>
     </header>
     <main>
       <router-view></router-view>
@@ -38,6 +59,7 @@ export default {
   name: 'App',
   data () {
     return {
+      toggle: false
     }
   },
   computed: {
@@ -51,12 +73,6 @@ export default {
         .then(() => {
           this.$router.push('/login')
         })
-    },
-    qwertY: function () {
-      console.log(this.$store.getters.isAuthenticated)
-      console.log(this.$store)
-      console.log(localStorage.getItem('access_token'))
-      //  v-if="divLogOut"
     }
   }
 }
@@ -77,65 +93,70 @@ export default {
     font-family: 'Butcherman';
     font-size: 30px;
     text-align: center;
-    background-color: rgb(231, 114, 114);
+    background-color: rgb(255, 255, 255);
     position: absolute;
     top: 0;
     width: 100%;
-    height: 8%;
     border-radius: 0;
+    border-bottom: 1px solid rgb(107, 126, 126);
   }
 
-  header div {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  .underHeader {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    height: 60px;
+    margin: 0 16px;;
   }
 
-  #buttLOG {
-    position: absolute;
-    top: 50%;
-    right: 10%;
-    transform: translate(-50%, -50%);
-  }
-
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
-
-  li {
-    float: left;
-  }
-
-  li a:hover, #dropdown:hover {
-    background-color: red;
-  }
-
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    top: 100%;
-    background-color: #f9f9f9;
-    min-width: 120px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-
-  .dropdown-content a {
-    color: black;
-    padding: 6px 8px;
+  .roulink {
     text-decoration: none;
-    display: block;
-    text-align: center;
   }
 
-  .dropdown-content a:hover {background-color: #357235}
+  .firstButt, .secondButt, .thirdButt {
+    border: 0px;
+    height: 40px;
+    display: inline-block;
+    border-radius: 4px;
+    padding: 0px;
+    font-size: 20px;
+    outline: none;
+    box-shadow: none;
+    cursor: pointer;
+    margin-top: 0px;
+    vertical-align: middle;
+    background-color: rgb(219, 51, 76);
+    color: rgb(0, 0, 0);
+    line-height: 40px;
+    width: 128px;
+    margin-right: 8px;
+    box-sizing: border-box;
+  }
 
-  #dropdown:hover .dropdown-content {
-    display: block;
+  .firstButt:hover, .secondButt:hover, .thirdButt:hover {
+    background-color: rgb(168, 28, 49);
+  }
+
+  #hideMenu {
+    position: absolute;
+    top: 60px;
+    right: 0px;
+    width: 150px;
+    background-color: azure;
+    border: 1px solid rgb(32, 29, 29);
+    border-radius: 8px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, .18);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    box-sizing: border-box;
+    font-size: 20px;
+  }
+
+  #hideMenu div {
+    margin-left: 10px;
+    padding: 0;
+    border: 1px solid rgb(32, 29, 29);
   }
 
   footer {
